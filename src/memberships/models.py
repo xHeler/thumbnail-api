2cf1fe9221081e2b3c1dbc73325f9da7ee7fc918
Bartosz <kwiatkowsky.kontakt@gmail.com>
2023-03-08 16:46:03 +0100

    style: remove unnecessary imports, restyle by isort

--- src/memberships/models.py
+++ src/memberships/models.py
@@ -1,16 +1,12 @@
 from django.contrib.auth import get_user_model
-from django.db import models
 from django.contrib.auth.models import Permission
+from django.db import models
 
 
 class Plan(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(blank=True, max_length=100)
-    default = models.BooleanField(
-        default=None,
-        unique=True,
-        null=True
-    )
+    default = models.BooleanField(default=None, unique=True, null=True)
     permissions = models.ManyToManyField(
         Permission,
         blank=True,
@@ -23,15 +19,9 @@ class Plan(models.Model):
 
 
 class Membership(models.Model):
-    user = models.OneToOneField(
-        get_user_model(),
-        on_delete=models.CASCADE
-    )
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
 
-    plan = models.ForeignKey(
-        Plan,
-        on_delete=models.CASCADE
-    )
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Membership ({self.plan.name}) {self.user}"
