8f5ca131a4b1432a8941c729c4ae2c6a33ec944b
Bartosz <kwiatkowsky.kontakt@gmail.com>
2023-03-09 10:54:08 +0100

    style: delete unnecessary import

--- config/wsgi.py
+++ config/wsgi.py
@@ -4,7 +4,6 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/gunicorn/
 """
-import os
 
 from configurations.wsgi import get_wsgi_application  # noqa
 
