bb707bba522f65c638c1ae6ef2b296278b035467
Bartosz <kwiatkowsky.kontakt@gmail.com>
2023-03-09 13:31:12 +0100

    refactor: create abstract model, move logic into utils

--- src/images/models/thumbnail.py
+++ src/images/models/thumbnail.py
@@ -1,52 +1,26 @@
-import sys
-import uuid
-from io import BytesIO
-
-from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.core.files.uploadedfile import InMemoryUploadedFile
-from django.core.validators import FileExtensionValidator
 from django.db import models
 from django.dispatch import receiver
-from PIL import Image
 
-from src.images.utils import file_generate_upload_path
+from src.images.utils import (
+    file_generate_upload_path,
+    get_image_validators,
+    get_resized_image,
+)
 
 
 class Thumbnail(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-
     image = models.ImageField(
-        upload_to=file_generate_upload_path,
-        validators=[
-            FileExtensionValidator(
-                allowed_extensions=settings.IMAGES_ALLOWED_EXTENSIONS
-            )
-        ],
+        upload_to=file_generate_upload_path, validators=[get_image_validators()]
     )
-
-    created_at = models.DateTimeField(db_index=True, auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     uploaded_by = models.ForeignKey(
         get_user_model(), null=True, on_delete=models.SET_NULL
     )
 
     def save(self, *args, **kwargs):
-        im = Image.open(self.image)
-        output = BytesIO()
-        im = im.resize((100, 100))
-        im.save(output, format="JPEG", quality=100)
-        output.seek(0)
-        self.image = InMemoryUploadedFile(
-            output,
-            "ImageField",
-            "%s.jpg" % self.image.name.split(".")[0],
-            "image/jpeg",
-            sys.getsizeof(output),
-            None,
-        )
-
+        size = (125, 125)
+        self.image = get_resized_image(self.image, size)
         super(Thumbnail, self).save(*args, **kwargs)
 
 
--- src/images/utils.py
+++ src/images/utils.py
@@ -1,5 +1,12 @@
 import pathlib
+import sys
+from io import BytesIO
 from uuid import uuid4
+
+from django.conf import settings
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.core.validators import FileExtensionValidator
+from PIL import Image
 
 
 def file_generate_name(original_file_name):
@@ -9,4 +16,24 @@ def file_generate_name(original_file_name):
 
 def file_generate_upload_path(instance, original_file_name):
     file_name = file_generate_name(original_file_name)
-    return f"{instance.uploaded_by.id}/{instance.id}/{file_name}"
+    return f"{instance.uploaded_by.id}/{file_name}"
+
+
+def get_image_validators():
+    return FileExtensionValidator(allowed_extensions=settings.IMAGES_ALLOWED_EXTENSIONS)
+
+
+def get_resized_image(orginal, size):
+    image = Image.open(orginal)
+    output = BytesIO()
+    image = image.resize(size)
+    image.save(output, format="JPEG", quality=100)
+    output.seek(0)
+    return InMemoryUploadedFile(
+        output,
+        "ImageField",
+        "%s.jpg" % orginal.name.split(".")[0],
+        "image/jpeg",
+        sys.getsizeof(output),
+        None,
+    )
